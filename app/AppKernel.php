9a3ba35fec50ed16851b34f203d4a93467ca24c2
mpetrunic <marin.petrunic@gmail.com>
2015-07-23 11:06:05 +0200

    Added Buzz bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Netgen\LiveVotingBundle\LiveVotingBundle(),
+            new Sensio\Bundle\BuzzBundle\SensioBuzzBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
