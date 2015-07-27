60a07cf4be01425ec59c0e2bbb0b10fbd48891eb
mpetrunic <marin.petrunic@gmail.com>
2015-07-27 13:14:47 +0200

    Included Memcache bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Netgen\LiveVotingBundle\LiveVotingBundle(),
             new Sensio\Bundle\BuzzBundle\SensioBuzzBundle(),
+            new Lsw\MemcacheBundle\LswMemcacheBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
